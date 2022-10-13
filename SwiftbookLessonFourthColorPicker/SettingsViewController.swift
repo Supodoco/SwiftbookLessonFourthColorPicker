5c15c1cd63a9d3f0ab96421e6183c3f0566ba310
sss <kirillku4mar24@gmail.com>
2022-10-13 13:57:38 +0300

    123

--- SwiftbookLessonFourthColorPicker/SettingsViewController.swift
+++ SwiftbookLessonFourthColorPicker/SettingsViewController.swift
@@ -167,7 +167,7 @@ extension UIViewController {
 extension UIView {
     func shake() {
         let animation = CAKeyframeAnimation(keyPath: "transform.translation.x")
-        animation.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.linear)
+        animation.timingFunction = CAMediaTimingFunction(name: .linear)
         animation.duration = 0.6
         animation.values = [-10.0, 10.0, -10.0, 10.0, -7.0, 7.0, -5.0, 5.0, 0.0 ]
         layer.add(animation, forKey: "shake")
